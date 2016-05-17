9beb20a09e78a6134e65c99a20dc6f83a3ada149
Mckenna Jones <mjones0014@gmail.com>
2016-05-16 17:48:07 -0700

    able to create partition and mount block device

--- linux-yocto-3.14/drivers/block/sbd.c
+++ linux-yocto-3.14/drivers/block/sbd.c
@@ -19,8 +19,17 @@
 #include <linux/blkdev.h>
 #include <linux/hdreg.h>
 
+// HW3
+#include <linux/crypto.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 static char *Version = "1.4";
+
+// HW3
+static char *encryption_key = "jonesmck";
+static int key_length = 8;
+struct crypto_cipher *cipher;
+module_param(encryption_key, charp, 0644);
 
 static int major_num = 0;
 module_param(major_num, int, 0);
@@ -44,49 +53,84 @@ static struct request_queue *Queue;
  * The internal representation of our device.
  */
 static struct sbd_device {
-  unsigned long size;
-  spinlock_t lock;
-  u8 *data;
-  struct gendisk *gd;
+	unsigned long size;
+	spinlock_t lock;
+	u8 *data;
+	struct gendisk *gd;
 } Device;
 
 /*
  * Handle an I/O request.
  */
 static void sbd_transfer(struct sbd_device *dev, sector_t sector,
-			 unsigned long nsect, char *buffer, int write) {
-  unsigned long offset = sector * logical_block_size;
-  unsigned long nbytes = nsect * logical_block_size;
+		unsigned long nsect, char *buffer, int write) {
+	unsigned long offset = sector * logical_block_size;
+	unsigned long nbytes = nsect * logical_block_size;
+	int i;
 
-  if ((offset + nbytes) > dev->size) {
-    printk (KERN_NOTICE "sbd: Beyond-end write (%ld %ld)\n", offset, nbytes);
-    return;
-  }
-  if (write)
-    memcpy(dev->data + offset, buffer, nbytes);
-  else
-    memcpy(buffer, dev->data + offset, nbytes);
+	/* HW3 */
+	u8 *source, *destination;
+
+	crypto_cipher_setkey(cipher, encryption_key, key_length);
+
+	if ((offset + nbytes) > dev->size) {
+		printk (KERN_NOTICE "sbd: Beyond-end write (%ld %ld)\n", offset, nbytes);
+		return;
+	}
+	if (write){
+		source = buffer;
+		destination = dev->data + offset;
+
+		for(i = 0; i < nbytes; i += crypto_cipher_blocksize(cipher)){
+			crypto_cipher_encrypt_one(cipher, destination + i, source + i);
+		}
+		printk("Decrypted:\n");
+		for(i = 0; i < nbytes; i++){
+			printk("%u", (unsigned) *source++);
+		}
+		printk("Encrypted:\n");
+		for(i = 0; i < nbytes; i++){
+			printk("%u", (unsigned) *destination);
+		}
+	}
+	else{
+		source = dev->data + offset;
+		destination = buffer;
+
+		for(i = 0; i < nbytes; i += crypto_cipher_blocksize(cipher)){
+			crypto_cipher_decrypt_one(cipher, destination + i, source + i);
+		}
+
+		printk("Decrypted:\n");
+		for(i = 0; i < nbytes; i++){
+			printk("%u", (unsigned) *source++);
+		}
+		printk("Encrypted:\n");
+		for(i = 0; i < nbytes; i++){
+			printk("%u", (unsigned) *destination);
+		}
+	}
 }
 
 static void sbd_request(struct request_queue *q) {
-  struct request *req;
+	struct request *req;
 
-  req = blk_fetch_request(q);
-  while (req != NULL) {
-    // blk_fs_request() was removed in 2.6.36 - many thanks to
-    // Christian Paro for the heads up and fix...
-    //if (!blk_fs_request(req)) {
-    if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
-      printk (KERN_NOTICE "Skip non-CMD request\n");
-      __blk_end_request_all(req, -EIO);
-      continue;
-    }
-    sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req),
-		 req->buffer, rq_data_dir(req));
-    if ( ! __blk_end_request_cur(req, 0) ) {
-      req = blk_fetch_request(q);
-    }
-  }
+	req = blk_fetch_request(q);
+	while (req != NULL) {
+		// blk_fs_request() was removed in 2.6.36 - many thanks to
+		// Christian Paro for the heads up and fix...
+		//if (!blk_fs_request(req)) {
+		if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
+			printk (KERN_NOTICE "Skip non-CMD request\n");
+			__blk_end_request_all(req, -EIO);
+			continue;
+		}
+		sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req),
+				req->buffer, rq_data_dir(req));
+		if ( ! __blk_end_request_cur(req, 0) ) {
+			req = blk_fetch_request(q);
+		}
+	}
 }
 
 /*
@@ -95,80 +139,86 @@ static void sbd_request(struct request_queue *q) {
  * use tools such as fdisk to partition the drive otherwise.
  */
 int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
-  long size;
+	long size;
 
-  /* We have no real geometry, of course, so make something up. */
-  size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
-  geo->cylinders = (size & ~0x3f) >> 6;
-  geo->heads = 4;
-  geo->sectors = 16;
-  geo->start = 0;
-  return 0;
+	/* We have no real geometry, of course, so make something up. */
+	size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
+	geo->cylinders = (size & ~0x3f) >> 6;
+	geo->heads = 4;
+	geo->sectors = 16;
+	geo->start = 0;
+	return 0;
 }
 
 /*
  * The device operations structure.
  */
 static struct block_device_operations sbd_ops = {
-  .owner  = THIS_MODULE,
-  .getgeo = sbd_getgeo
+		.owner  = THIS_MODULE,
+		.getgeo = sbd_getgeo
 };
 
 static int __init sbd_init(void) {
-  /*
-   * Set up our internal device.
-   */
-  Device.size = nsectors * logical_block_size;
-  spin_lock_init(&Device.lock);
-  Device.data = vmalloc(Device.size);
-  if (Device.data == NULL)
-    return -ENOMEM;
-  /*
-   * Get a request queue.
-   */
-  Queue = blk_init_queue(sbd_request, &Device.lock);
-  if (Queue == NULL)
-    goto out;
-  blk_queue_logical_block_size(Queue, logical_block_size);
-  /*
-   * Get registered.
-   */
-  major_num = register_blkdev(major_num, "sbd");
-  if (major_num < 0) {
-    printk(KERN_WARNING "sbd: unable to get major number\n");
-    goto out;
-  }
-  /*
-   * And the gendisk structure.
-   */
-  Device.gd = alloc_disk(16);
-  if (!Device.gd)
-    goto out_unregister;
-  Device.gd->major = major_num;
-  Device.gd->first_minor = 0;
-  Device.gd->fops = &sbd_ops;
-  Device.gd->private_data = &Device;
-  strcpy(Device.gd->disk_name, "sbd0");
-  set_capacity(Device.gd, nsectors);
-  Device.gd->queue = Queue;
-  add_disk(Device.gd);
 
-  return 0;
+	/* HW 3 */
+	cipher = crypto_alloc_cipher("aes", 0, 0);
 
- out_unregister:
-  unregister_blkdev(major_num, "sbd");
- out:
-  vfree(Device.data);
-  return -ENOMEM;
+	/*
+	 * Set up our internal device.
+	 */
+	Device.size = nsectors * logical_block_size;
+	spin_lock_init(&Device.lock);
+	Device.data = vmalloc(Device.size);
+	if (Device.data == NULL)
+		return -ENOMEM;
+	/*
+	 * Get a request queue.
+	 */
+	Queue = blk_init_queue(sbd_request, &Device.lock);
+	if (Queue == NULL)
+		goto out;
+	blk_queue_logical_block_size(Queue, logical_block_size);
+	/*
+	 * Get registered.
+	 */
+	major_num = register_blkdev(major_num, "sbd");
+	if (major_num < 0) {
+		printk(KERN_WARNING "sbd: unable to get major number\n");
+		goto out;
+	}
+	/*
+	 * And the gendisk structure.
+	 */
+	Device.gd = alloc_disk(16);
+	if (!Device.gd)
+		goto out_unregister;
+	Device.gd->major = major_num;
+	Device.gd->first_minor = 0;
+	Device.gd->fops = &sbd_ops;
+	Device.gd->private_data = &Device;
+	strcpy(Device.gd->disk_name, "sbd0");
+	set_capacity(Device.gd, nsectors);
+	Device.gd->queue = Queue;
+	add_disk(Device.gd);
+
+	return 0;
+
+out_unregister:
+	unregister_blkdev(major_num, "sbd");
+out:
+	vfree(Device.data);
+	return -ENOMEM;
 }
 
 static void __exit sbd_exit(void)
 {
-  del_gendisk(Device.gd);
-  put_disk(Device.gd);
-  unregister_blkdev(major_num, "sbd");
-  blk_cleanup_queue(Queue);
-  vfree(Device.data);
+	// HW 3
+	crypto_free_cipher(cipher);
+	del_gendisk(Device.gd);
+	put_disk(Device.gd);
+	unregister_blkdev(major_num, "sbd");
+	blk_cleanup_queue(Queue);
+	vfree(Device.data);
 }
 
 module_init(sbd_init);
